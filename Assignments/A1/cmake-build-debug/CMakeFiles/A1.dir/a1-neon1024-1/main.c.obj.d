e37f2624b9203bbdc340490f803612fc952113a7
neon1024 <robertscumnpu@gmail.com>
2023-03-13 18:52:58 +0200

    run

---
+++ Assignments/A1/cmake-build-debug/CMakeFiles/A1.dir/a1-neon1024-1/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/A1.dir/a1-neon1024-1/main.c.obj: \
+ C:\Users\rober\Desktop\Coding\ Spaces\UBB\ Coding\ Space\Object-Oriented-Programming\Assignments\A1\a1-neon1024-1\main.c \
+ C:\Users\rober\Desktop\Coding\ Spaces\UBB\ Coding\ Space\Object-Oriented-Programming\Assignments\A1\a1-neon1024-1\main.h \
+ C:/msys64/mingw64/include/stdio.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/stdio_s.h
---
+++ Assignments/A1/cmake-build-debug/CMakeFiles/A1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "A1.exe"
+  "A1.exe.manifest"
+  "A1.pdb"
+  "CMakeFiles/A1.dir/a1-neon1024-1/main.c.obj"
+  "CMakeFiles/A1.dir/a1-neon1024-1/main.c.obj.d"
+  "libA1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
